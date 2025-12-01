ae8e3ff81d59f5ac8811ac47c330b41dd548b982
Shon Thomas <shonpt@outlook.com>
2025-12-01 14:46:00 -0900

    Remove unused json-to-graphql and resolve-builder module files

--- src/infer/json-to-graphql.ts
+++
@@ -1,5 +0,0 @@
-/**
- * json-to-graphql Module
- * This module exports functions to convert cosmosdb inferred JSON schemas to GraphQL schemas.
- * @module
- */
--- src/infer/resolve-builder.ts
+++
@@ -1,5 +0,0 @@
-/**
- * resolve-builder Module
- * This module exports functions to build resolvers for GraphQL schemas based on inferred CosmosDB schemas.
- * @module
- */
