cbfcc202aed4a65fa164d504bd05434e5d26e4c2
Shon Thomas <shonpt@outlook.com>
2025-12-02 08:22:30 -0900

    chore: clean-up exports in main module

--- mod.ts
+++ mod.ts
@@ -14,15 +14,4 @@ export * from './src/handler/mod.ts'
 
 export * from './src/cache/mod.ts'
 
-export { generateSDL } from './src/adapters/generic.ts'
-export { loadCosmosDBSubgraph } from './src/adapters/mesh.ts'
-export { buildCoreSchema } from './src/adapters/core.ts'
-export { createApolloAdapter } from './src/adapters/apollo.ts'
-export { createYogaAdapter } from './src/adapters/yoga.ts'
-export { uploadToHive } from './src/adapters/hive.ts'
-
-export type { GenericSDLConfig, GenericSDLResult } from './src/adapters/types.ts'
-export type { CoreSchemaResult } from './src/adapters/core.ts'
-export type { ApolloAdapterConfig, ApolloAdapterResult, ApolloContext } from './src/adapters/apollo.ts'
-export type { YogaAdapterConfig, YogaAdapterResult, YogaContext } from './src/adapters/yoga.ts'
-export type { HiveAdapterConfig, HiveAdapterResult } from './src/adapters/hive.ts'
+export * from './src/adapters/mod.ts'
